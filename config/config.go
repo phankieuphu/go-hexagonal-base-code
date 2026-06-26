49f85cdffd44b3dc62ae01599381730bb5481f92
Patrick Phan <phankieuphu@gmail.com>
2026-06-26 15:27:19 +0700

    [ADD] Kafka, cache , API

--- config/config.go
+++ config/config.go
@@ -2,12 +2,16 @@ package config
 
 import (
 	"os"
+	"strconv"
 	"time"
 )
 
 type Config struct {
 	AWS
 	Database
+	API
+	Kafka
+	Redis
 }
 
 type AWS struct {
@@ -18,6 +22,7 @@ type AWS struct {
 type SQSTopic struct {
 	Account string
 }
+
 type Database struct {
 	Host            string
 	Port            int
@@ -30,8 +35,31 @@ type Database struct {
 	Driver          string
 }
 
-func LoadConfig() *Config {
+type API struct {
+	Port         string
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+}
 
+type Kafka struct {
+	Brokers       []string
+	ProducerTopic string
+	ConsumerTopic string
+	ConsumerGroup string
+}
+
+type Redis struct {
+	Host     string
+	Port     string
+	Password string
+	DB       int
+}
+
+func (r Redis) Addr() string {
+	return r.Host + ":" + r.Port
+}
+
+func LoadConfig() *Config {
 	return &Config{
 		AWS: AWS{
 			Region: GetEnv("AWS_REGION", "ap-southeast-1"),
@@ -39,8 +67,23 @@ func LoadConfig() *Config {
 				Account: GetEnv("ACCOUNTING_SQS", ""),
 			},
 		},
-		Database: Database{
-			// AccountTableName: GetEnv("ACCOUNT_ENTRY_TABLE", "accounting_account_entry"),
+		Database: Database{},
+		API: API{
+			Port:         GetEnv("API_PORT", "8080"),
+			ReadTimeout:  time.Duration(getEnvInt("API_READ_TIMEOUT_SEC", 30)) * time.Second,
+			WriteTimeout: time.Duration(getEnvInt("API_WRITE_TIMEOUT_SEC", 30)) * time.Second,
+		},
+		Kafka: Kafka{
+			Brokers:       []string{GetEnv("KAFKA_BROKERS", "localhost:9092")},
+			ProducerTopic: GetEnv("KAFKA_PRODUCER_TOPIC", "account.events"),
+			ConsumerTopic: GetEnv("KAFKA_CONSUMER_TOPIC", "account.events"),
+			ConsumerGroup: GetEnv("KAFKA_CONSUMER_GROUP", "account-service"),
+		},
+		Redis: Redis{
+			Host:     GetEnv("REDIS_HOST", "localhost"),
+			Port:     GetEnv("REDIS_PORT", "6379"),
+			Password: GetEnv("REDIS_PASSWORD", ""),
+			DB:       getEnvInt("REDIS_DB", 0),
 		},
 	}
 }
@@ -51,5 +94,16 @@ func GetEnv(key string, defaultValue string) string {
 		return result
 	}
 	return defaultValue
+}
 
+func getEnvInt(key string, defaultValue int) int {
+	val := os.Getenv(key)
+	if val == "" {
+		return defaultValue
+	}
+	n, err := strconv.Atoi(val)
+	if err != nil {
+		return defaultValue
+	}
+	return n
 }
